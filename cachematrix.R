6446fbf95bb366165d3e51fd550d58e4137599bc
MelvinLopez <melvin.lopez314@gmail.com>
2020-05-31 09:47:23 -0400

    Removed hash key from comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,5 @@
 ## Put comments here that give an overall description of what your
+## MyHash: 7f657dd22ac20d22698c53b23f0057e1a12c09b7
 ## functions do
 
 ## Write a short comment describing this function
